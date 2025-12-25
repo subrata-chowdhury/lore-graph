2077a8d7d44de65896d231f326a4a649dcca75bf
Subrata Chowdhury <subratachowdhurysuper@gmail.com>
2025-12-25 09:58:11 +0530

    Revert "configure for cloudeflare deplyemnt"
    This reverts commit 9cefbf3862ae20940986b2f8a00fb82bbcd46c3b.

--- proxy.ts
+++ proxy.ts
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 import verifyToken from '@/libs/tokenVerify';
 import { cookies } from 'next/headers';
-// import User from './models/user';
-// import dbConnect from './config/db';
+import User from './models/user';
+import dbConnect from './config/db';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     const excludeTokenVerification = ['/api/auth/login', '/api/auth/signup', '/api/admin/auth/login', '/api/collector/auth/login', '/api/tests', '/api/labs', '/api/cart/count', '/api/contact'];
     const excludeTokenVerificationPatterns = [/^\/api\/tests\/.*/, /^\/api\/labs\/.*/];
     if (excludeTokenVerification.includes(request.nextUrl.pathname) || excludeTokenVerificationPatterns.some(pattern => pattern.test(request.nextUrl.pathname))) {
@@ -29,7 +29,7 @@ export async function middleware(request: NextRequest) {
             break;
     }
 
-    // await dbConnect();
+    await dbConnect();
 
     let user: { id: string, verified: string } | boolean = false;
     if (token) {
@@ -38,7 +38,7 @@ export async function middleware(request: NextRequest) {
             return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
         }
 
-        // const existingUser = await User.findById(user.id);
+        const existingUser = await User.findById(user.id);
         const requestHeaders = new Headers(request.headers);
         requestHeaders.set('x-user', user.id);
 
